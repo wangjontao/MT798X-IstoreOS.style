28b5d092524e617ed11b54100ee782cc1d8d974e
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-01-29 18:34:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,8 +22,8 @@ sed -i '3i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt
 sed -i '4i src-git OpenClash https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
 sed -i '5i src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
 sed -i '6i src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main' feeds.conf.default
-sed -i '7i src-git daed https://github.com/QiuSimons/luci-app-daed.git;main' feeds.conf.default
-sed -i '8i src-git ssr-plus https://github.com/fw876/helloworld.git;main' feeds.conf.default
+sed -i '7i src-git daed https://github.com/QiuSimons/luci-app-daed.git;master' feeds.conf.default
+sed -i '8i src-git ssr-plus https://github.com/fw876/helloworld.git;master' feeds.conf.default
 
 # 插件添加
 git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog package/watchdog
